9ef75deac13a25e02b20c84b7fe9ece14c7def25
Junhui Cho <junhuicho8@gmail.com>
2017-09-14 00:40:44 +0900

    removed helloworld Test code.

--- src/main/java/com/junhui00/blog/SampleController.java
+++ src/main/java/com/junhui00/blog/SampleController.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -17,42 +16,8 @@ import java.util.List;
 public class SampleController {
 
     @Autowired
-    private HelloDao helloDao;
-
-    @Autowired
     private PostDao postDao;
-
-    @RequestMapping("/add")
-    public @ResponseBody Hello add(Hello hello) {
-
-        Hello helloData = helloDao.save(hello);
-
-        return helloData;
-    }
-
-    @RequestMapping("/list")
-    public @ResponseBody List<Hello> list(Model model) {
-
-        List<Hello> helloList = helloDao.findAll();
-
-        return helloList;
-    }
-
-    @RequestMapping("/")
-    public String index() {
-        return "helloworld!";
-    }
-    @ResponseBody
-    String home() {
-        return "Hello Wffffffforld!";
-    }
-
-    @RequestMapping("/hello")
-    public String index(Model model) {
-        model.addAttribute("name", "123");
-        return "hello";
-    }
-
+    
     @RequestMapping("/index")
     public String posts(Model model){
         List<Post> postList = postDao.findAll();
