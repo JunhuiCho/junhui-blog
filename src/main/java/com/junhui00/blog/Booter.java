c348eb4fb6f88642ecadfa51efd92ea7393663bb
Junhui Cho <junhuicho8@gmail.com>
2017-10-05 16:38:33 +0900

    destribution verison

--- src/main/java/com/junhui00/blog/Booter.java
+++ src/main/java/com/junhui00/blog/Booter.java
@@ -2,9 +2,10 @@ package com.junhui00.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Booter {
+public class Booter extends SpringBootServletInitializer{
     public static void main(String[] args){
         SpringApplication.run(Booter.class, args);
     }
